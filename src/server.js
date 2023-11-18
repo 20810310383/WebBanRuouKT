d61bc620cd50e404e28e96d439602bfe4b587aaa
Khac tu <dokhactu1822@gmail.com>
2023-11-18 15:32:35 +0700

    update

--- src/server.js
+++ src/server.js
@@ -2,22 +2,29 @@ require('dotenv').config();
 const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const apiRoutes = require('./routes/api');
 const connection = require('./config/database');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
+
+const fileUpload = require('express-fileupload');
+
 
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
-configViewEngine(app);
+// default options
+app.use(fileUpload());
 
-// khai bao route
+configViewEngine(app);
 
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// khai bao route
 app.use('/', webRoutes);
+app.use('/v1/api/', apiRoutes);
 
 
 // test connection
